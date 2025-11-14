a95fa2241f0065944f8af2c2c9b3afa6efcfcb76
mobu-unsw <mobu-unsw@users.noreply.github.com>
2025-11-14 11:27:26 +0800

    测试北京2

--- simulink/MyLib/slblocks.m
+++
@@ -1,8 +0,0 @@
-function blkStruct = slblocks
-% 指定库在浏览器中的显示信息
-
-Browser.Library = 'MyLib';  % 您的库文件名（不含扩展名）
-Browser.Name    = 'My Library';  % 在浏览器中显示的名称
-Browser.IsFlat  = 0;  % 0表示使用层次结构，1表示扁平结构
-
-blkStruct.Browser = Browser;
